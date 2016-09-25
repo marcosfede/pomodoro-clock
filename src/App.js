3ccf0802882e06bef90e84464a89a83a0841e1b2
Federico Marcos <marcosfede@gmail.com>
2016-09-25 00:30:27 -0300

    commit in the middle in case I want to  go back, before deleting old timer code and moving to flipclock

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import '../node_modules/flipclock/compiled/flipclock.css'
 import './App.css'
 // import './components/FlipClock.css'
 import Topbar from './components/Topbar'
@@ -9,7 +8,7 @@ import FlatButton from 'material-ui/FlatButton'
 import $ from 'jquery'
 window.$ = $
 window.jQuery = $
-window.FlipClock = require('../node_modules/flipclock/compiled/flipclock')
+window.FlipClock = require('../FlipClock/dev/dist/flipclock')
 
 const styles = {
   App: {
@@ -42,8 +41,8 @@ class App extends Component {
       Notification.requestPermission()
     }
     this.initCounter()
-    clock.stop()
   }
+
   slideUp = () => {
     $('#text').slideUp(300)
   }
@@ -52,32 +51,94 @@ class App extends Component {
   }
 
   initCounter = () => {
-    // FlipClock.FedeClockFace = FlipClock.MinuteCounterFace.extend({
-    //   showSeconds: true,
-    // })
     window.clock = $('.countdown').FlipClock(this.state.timer,{
+      clockFace: 'MinuteCounter',
       countdown: true,
-      clockFace: 'MinuteCounter'
-    })
+      autoStart: false,
+      onStart: function () {
+        console.log('start')
+      },
+      onReset:  () => {console.log('Reset')},
+      onStop:  () => {
+        if (window.clock.getFaceValue()===0){
+          this.timerReached0()
+        }
+      },
+    }).setCountdown(true).stop().reset()
 
+  }
+
+  timerReached0 = () => {
+    if (this.state.currentBlock === 'session'){
+      this.setState({currentBlock: 'break'})
+      this.playSound()
+      this.showNotification()
+      clock.setFaceValue(this.state.breakTime).start()
+    }
   }
 
   playPause = () => {
     if (this.state.status === 'stopped') {
       this.setState({status: 'running', timer: this.state.sessionTime})
-      clock.start()
+      window.clock.start()
       this.timeout()
     }
     else if (this.state.status === 'paused' && this.state.timer > 0) {
       this.setState({status: 'running'})
-      clock.start()
+      window.clock.start()
       this.timeout()
     }
     else {
       this.setState({status: 'paused'})
-      clock.stop()
+      window.clock.stop()
     }
   }
+  resetTimer = () => {
+    this.setState({timer: this.state.sessionTime, status: 'stopped', currentBlock: 'session'})
+    window.clock.stop()
+    window.clock.setFaceValue(this.state.sessionTime)
+    window.setTimeout(() => this.setState({timer: this.state.sessionTime}), 1000)
+  }
+  playSound = () => {
+    let audio = new Audio('https://mca62511.github.io/pomodoro/audio/ding.mp3')
+    audio.play()
+  }
+  showNotification = () => {
+    if (Notification.permission !== 'granted')
+      Notification.requestPermission()
+    else {
+      var notification = new Notification('Time is up!', {
+        icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+        body: "Time is up!"
+      })
+      notification.onclick = function () {
+        window.open('http://stackoverflow.com/a/13328397/1269037')
+      }
+    }
+  }
+  handleSound = (event, toggle) => {
+    this.setState({ toggledSound: toggle})}
+  handleNotifications = (event, toggle) => {
+    this.setState({ toggledNotifications: toggle})}
+  handleRepeat = (event, toggle) => {
+    this.setState({ toggledRepeat: toggle})}
+
+  // this block will be deleted in the furure
+
+  secToMinutes = (sec) => {
+    let min = Math.floor(sec / 60)
+    let remainingsec = sec % 60
+    let zeroremainingsec = remainingsec < 10 ? '0' + remainingsec : remainingsec
+    return `${min}:${zeroremainingsec}`
+  }
+
+  timeout = () => {
+    window.setTimeout(() => {
+      this.setState({timer: this.state.timer - 1})
+      this.stopOrTimeout()
+    }, 1000)
+  }
+
   stopOrTimeout = () => {
     // check if the user hasn't stoped the timer
     if (this.state.status === 'running') {
@@ -106,61 +167,18 @@ class App extends Component {
       }
     }
   }
-  timeout = () => {
-    window.setTimeout(() => {
-      this.setState({timer: this.state.timer - 1})
-      this.stopOrTimeout()
-    }, 1000)
-  }
-  resetTimer = () => {
-    this.setState({timer: this.state.sessionTime, status: 'stopped', currentBlock: 'session'})
-    clock.stop()
-    clock.setTime(this.state.sessionTime)
-    window.setTimeout(() => this.setState({timer: this.state.sessionTime}), 1000)
-  }
-  secToMinutes = (sec) => {
-    let min = Math.floor(sec / 60)
-    let remainingsec = sec % 60
-    let zeroremainingsec = remainingsec < 10 ? '0' + remainingsec : remainingsec
-    return `${min}:${zeroremainingsec}`
-  }
-  playSound = () => {
-    let audio = new Audio('https://mca62511.github.io/pomodoro/audio/ding.mp3')
-    audio.play()
-  }
-  showNotification = () => {
-    if (Notification.permission !== 'granted')
-      Notification.requestPermission()
-    else {
-      var notification = new Notification('Time is up!', {
-        icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
-        body: "Time is up!"
-      })
-      notification.onclick = function () {
-        window.open('http://stackoverflow.com/a/13328397/1269037')
-      }
-    }
-  }
-  handleSound = (event, toggle) => {
-    this.setState({ toggledSound: toggle})}
-  handleNotifications = (event, toggle) => {
-    this.setState({ toggledNotifications: toggle})}
-  handleRepeat = (event, toggle) => {
-    this.setState({ toggledRepeat: toggle})}
-
+  //
   render () {
     return (
       <div style={styles} id='App' className='App'>
         <Topbar/>
         <div id='content'>
-          <div className="countdown-wrapper">
-            <div className="countdown flip-clock-wrapper">
-
-            </div>
-          </div>
           <Card id='card' zDepth={4}>
             <CardText id='timer'>
-              <span>{this.secToMinutes(this.state.timer)}</span>
+              <div className="countdown-wrapper">
+                <div className="countdown flip-clock-wrapper">
+                </div>
+              </div>
             </CardText>
             <Controls
               sessionTime={this.state.sessionTime}
---
+++ todo.md
@@ -0,0 +1,7 @@
+Todo:
+
+- when the clock reaches 0 from session it goes to break
+- if the clock reaches 0 from break, starts again if repeat is enabled
+- sound and notifications.
+
+what I have to do is invoke a function when the timer reaches 0.
